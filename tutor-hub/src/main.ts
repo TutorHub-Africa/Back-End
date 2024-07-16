6b7aed38cf20aeba1b422527d0b2fceaf51c24b6
Eba Adisu <ebaadisu2@gmail.com>
2024-07-16 12:13:48 +0300

    Add cors package and enable CORS

--- tutor-hub/src/main.ts
+++ tutor-hub/src/main.ts
@@ -4,9 +4,12 @@ import * as dotenv from 'dotenv';
 
 async function bootstrap() {
   dotenv.config();
-
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
-  app.enableCors();
+  // app.enableCors({
+  //   origin: 'http://localhost:5173', // URL of your React frontend
+  //   credentials: true, // Allow cookies to be sent
+  // });
+  // app.enableCors();
 }
 bootstrap();
